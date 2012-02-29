01c2c505650924ec7eb810a1ac025191a3bcddfe
Max Malm <benjick@dumfan.net>
2012-02-29 14:48:56 +0100

    Added text to promote widgets

--- footer.php
+++ footer.php
@@ -9,6 +9,8 @@
 			<div class="row">
 			
 					<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer Sidebar')) : ?>
+					<li><h4><?php _e('Hey! You!', 'Foundation') ?></h4></li>
+					<li><?php _e('You should like, so test out this dynamic sidebar. Check it out in Appearance > Widgets!', 'Foundation') ?></li>
 					<?php endif; ?>
 				
 			</div>
